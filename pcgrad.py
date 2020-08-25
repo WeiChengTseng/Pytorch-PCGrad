bf4d0126f29a153dff67a038ea5d708ef30b3652
Wei-Cheng Tseng <ethanweichengtseng@gmail.com>
2020-08-25 13:55:38 +0800

    update pcgrad

---
+++ pcgrad.py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import pdb
+import numpy as np
+import copy
+import random
+
+
+class PCGrad():
+    def __init__(self, optimizer):
+        self._optim = optimizer
+        return
+
+    @property
+    def optimizer(self):
+        return self._optim
+
+    def zero_grad(self):
+        return self._optim.zero_grad()
+
+    def step(self):
+        return self._optim.step()
+
+    def pc_backward(self, objectives):
+        '''
+        input:
+        - objectives: a list of objectives
+        '''
+
+        grads, shapes = self._pack_grad(objectives)
+        pc_grad = self._project_conflicting(grads)
+        pc_grad = self._unflatten_grad(pc_grad, shapes[0])
+        self._set_grad(pc_grad)
+        return
+
+    def _project_conflicting(self, grads, shapes=None):
+        pc_grad, num_task = copy.deepcopy(grads), len(grads)
+        for g_i in pc_grad:
+            random.shuffle(grads)
+            for g_j in grads:
+                g_i_g_j = torch.dot(g_i, g_j)
+                if g_i_g_j < 0:
+                    g_i -= (g_i_g_j) * g_j / (g_j.norm()**2)
+        pc_grad = torch.cat([g.unsqueeze(0) for g in pc_grad]).sum(dim=0)
+        return pc_grad
+
+    def _set_grad(self, grads):
+        idx = 0
+        for group in self._optim.param_groups:
+            for p in group['params']:
+                if p.grad is None: continue
+                p.grad = grads[idx]
+                idx += 1
+        return
+
+    def _pack_grad(self, objectives):
+        grads, shapes = [], []
+        for obj in objectives:
+            self._optim.zero_grad()
+            obj.backward(retain_graph=True)
+            grad, shape = self._retrieve_grad()
+            grads.append(self._flatten_grad(grad, shape))
+            shapes.append(shape)
+        return grads, shapes
+
+    def _unflatten_grad(self, grads, shapes):
+        unflatten_grad, idx = [], 0
+        for shape in shapes:
+            length = np.prod(shape)
+            unflatten_grad.append(grads[idx:idx + length].view(shape))
+            idx += length
+        return unflatten_grad
+
+    def _flatten_grad(self, grads, shapes):
+        flatten_grad = torch.cat([g.flatten() for g in grads])
+        return flatten_grad
+
+    def _retrieve_grad(self):
+        '''
+        get the gradient of the parameters of the network.
+        
+        output:
+        - grad: a list of the gradient of the parameters
+        - shape: a list of the shape of the parameters
+        '''
+
+        grad, shape = [], []
+        for group in self._optim.param_groups:
+            for p in group['params']:
+                if p.grad is None: continue
+                shape.append(p.grad.shape)
+                grad.append(p.grad.clone())
+        return grad, shape
+
+
+class TestNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self._linear = nn.Linear(3, 4)
+
+    def forward(self, x):
+        return self._linear(x)
+
+
+if __name__ == '__main__':
+    torch.manual_seed(4)
+    x, y = torch.randn(2, 3), torch.randn(2, 4)
+    net = TestNet()
+    y_pred = net(x)
+    pc_adam = PCGrad(optim.Adam(net.parameters()))
+    pc_adam.zero_grad()
+    loss1_fn, loss2_fn = nn.L1Loss(), nn.MSELoss()
+    loss1, loss2 = loss1_fn(y_pred, y), loss2_fn(y_pred, y)
+
+    pc_adam.pc_backward([loss1, loss2])
