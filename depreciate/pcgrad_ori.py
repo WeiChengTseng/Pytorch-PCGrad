1e6e4e57d22cfc5d66f28af5149d98d5fce87533
Wei-Cheng Tseng <ethanweichengtseng@gmail.com>
2021-01-12 17:23:59 +0800

    tackle the multi-head scenario

--- depreciate/pcgrad_ori.py
+++ depreciate/pcgrad_ori.py
@@ -22,7 +22,7 @@ class PCGrad():
         clear the gradient of the parameters
         '''
 
-        return self._optim.zero_grad(set_to_none=True)
+        return self._optim.zero_grad()
 
     def step(self):
         '''
@@ -33,20 +33,17 @@ class PCGrad():
 
     def pc_backward(self, objectives):
         '''
-        calculate the gradient of the parameters
-
         input:
         - objectives: a list of objectives
         '''
 
-        grads, shapes, has_grads = self._pack_grad(objectives)
-        pc_grad = self._project_conflicting(grads, has_grads)
+        grads, shapes = self._pack_grad(objectives)
+        pc_grad = self._project_conflicting(grads)
         pc_grad = self._unflatten_grad(pc_grad, shapes[0])
         self._set_grad(pc_grad)
         return
 
-    def _project_conflicting(self, grads, has_grads, shapes=None):
-        shared = torch.stack(has_grads).prod(0).bool()
+    def _project_conflicting(self, grads, shapes=None):
         pc_grad, num_task = copy.deepcopy(grads), len(grads)
         for g_i in pc_grad:
             random.shuffle(grads)
@@ -54,45 +51,27 @@ class PCGrad():
                 g_i_g_j = torch.dot(g_i, g_j)
                 if g_i_g_j < 0:
                     g_i -= (g_i_g_j) * g_j / (g_j.norm()**2)
-        merged_grad = torch.zeros_like(grads[0]).to(grads[0].device)
-        merged_grad[shared] = torch.stack([g[shared]
-                                           for g in pc_grad]).mean(dim=0)
-        merged_grad[~shared] = torch.stack([g[~shared]
-                                            for g in pc_grad]).sum(dim=0)
-        return merged_grad
+        pc_grad = torch.stack(pc_grad).mean(dim=0)
+        return pc_grad
 
     def _set_grad(self, grads):
-        '''
-        set the modified gradients to the network
-        '''
-
         idx = 0
         for group in self._optim.param_groups:
             for p in group['params']:
-                # if p.grad is None: continue
+                if p.grad is None: continue
                 p.grad = grads[idx]
                 idx += 1
         return
 
     def _pack_grad(self, objectives):
-        '''
-        pack the gradient of the parameters of the network for each objective
-        
-        output:
-        - grad: a list of the gradient of the parameters
-        - shape: a list of the shape of the parameters
-        - has_grad: a list of mask represent whether the parameter has gradient
-        '''
-
-        grads, shapes, has_grads = [], [], []
+        grads, shapes = [], []
         for obj in objectives:
-            self._optim.zero_grad(set_to_none=True)
+            self._optim.zero_grad()
             obj.backward(retain_graph=True)
-            grad, shape, has_grad = self._retrieve_grad()
+            grad, shape = self._retrieve_grad()
             grads.append(self._flatten_grad(grad, shape))
-            has_grads.append(self._flatten_grad(has_grad, shape))
             shapes.append(shape)
-        return grads, shapes, has_grads
+        return grads, shapes
 
     def _unflatten_grad(self, grads, shapes):
         unflatten_grad, idx = [], 0
@@ -108,29 +87,20 @@ class PCGrad():
 
     def _retrieve_grad(self):
         '''
-        get the gradient of the parameters of the network with specific 
-        objective
+        get the gradient of the parameters of the network.
         
         output:
         - grad: a list of the gradient of the parameters
         - shape: a list of the shape of the parameters
-        - has_grad: a list of mask represent whether the parameter has gradient
         '''
 
-        grad, shape, has_grad = [], [], []
+        grad, shape = [], []
         for group in self._optim.param_groups:
             for p in group['params']:
-                # if p.grad is None: continue
-                # tackle the multi-head scenario
-                if p.grad is None:
-                    shape.append(p.shape)
-                    grad.append(torch.zeros_like(p).to(p.device))
-                    has_grad.append(torch.zeros_like(p).to(p.device))
-                    continue
+                if p.grad is None: continue
                 shape.append(p.grad.shape)
                 grad.append(p.grad.clone())
-                has_grad.append(torch.ones_like(p).to(p.device))
-        return grad, shape, has_grad
+        return grad, shape
 
 
 class TestNet(nn.Module):
@@ -170,18 +140,15 @@ if __name__ == '__main__':
     for p in net.parameters():
         print(p.grad)
 
-    print('-' * 80)
     # seperated shared network test
 
-    torch.manual_seed(4)
-    x, y = torch.randn(2, 3), torch.randn(2, 4)
-    net = MultiHeadTestNet()
-    y_pred_1, y_pred_2 = net(x)
-    pc_adam = PCGrad(optim.Adam(net.parameters()))
-    pc_adam.zero_grad()
-    loss1_fn, loss2_fn = nn.MSELoss(), nn.MSELoss()
-    loss1, loss2 = loss1_fn(y_pred_1, y), loss2_fn(y_pred_2, y)
+    # torch.manual_seed(4)
+    # x, y = torch.randn(2, 3), torch.randn(2, 4)
+    # net = MultiHeadTestNet()
+    # y_pred_1, y_pred_2 = net(x)
+    # pc_adam = PCGrad(optim.Adam(net.parameters()))
+    # pc_adam.zero_grad()
+    # loss1_fn, loss2_fn = nn.L1Loss(), nn.MSELoss()
+    # loss1, loss2 = loss1_fn(y_pred_1, y), loss2_fn(y_pred_2, y)
 
-    pc_adam.pc_backward([loss1, loss2])
-    for p in net.parameters():
-        print(p.grad)
+    # pc_adam.pc_backward([loss1, loss2])
